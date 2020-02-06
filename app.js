4da71f533d55578d4345197136dd8d3a76032225
hil20n <johhilden@gmail.com>
2020-02-06 14:11:23 +0200

    testaus2

--- app.js
+++ app.js
@@ -6,17 +6,36 @@ var kuvasijainti = document.getElementById("kuvasijainti");
 var kameracanvas = document.getElementById("kameracanvas");
 var laukaisin = document.getElementById("laukaisin");
 
+
+
 function cameraStarttaa(){
     navigator.mediaDevices.getUserMedia(alkuarvot).then(function(kuvavirta){
         kamerakuva.srcObject = kuvavirta;
+       
     })  
     .catch(function(error){console.log("hupsista")})
 }
 
 window.addEventListener("load", cameraStarttaa);
-    // hakee media laitteita joita laitteeessa on, tässä kamera ja siitä video
+    //get UserMedia hakee media laitteita joita laitteeessa on, tässä kamera ja siitä video
         //kuvavirta = stream
         //srcObject= sourceObject
         //jos onnistuu ottaa kuvavirtaa videota ja tallennetaan kameran kuva html video elelmenttiin
         // eli kamerakuva johon tulee kuvavirta jos ei onnistu tulostetaan virheilmoitus
     
+    //laukaisin.onclick = cameraStarttaa;
+    laukaisin.onclick = function(){
+        kameracanvas.width = kamerakuva.videoWidth; //kuvan leveys ja pituus samat kuin videolla
+        kameracanvas.height = kamerakuva.videoHeight;
+
+        kameracanvas.getContext("2d").drawImage(kamerakuva, 0,0); //painalluksen kohdalla oleva kuva kuvavirrassa piirtyy canvakselle
+        kuvasijainti.src = kameracanvas.toDataURL("image/png"); //tallentaa kuvan tietokoneelle
+        kuvasijainti.id = "kuvaMuutos";  //tehdään yksittäisestä kuvasta pienempi, iso canvas piilotettu, muuten tulisi kaksi kuvaa
+        console.log("täällä ollaan");
+        /*kuvasijainti.style.width = "100px";
+        kuvasijainti.style.height = "100px"; */
+    }
+
+    /*function nappulanPainallus(){
+        kameracanvas.getContext("2d").drawImage(kamerakuva, 0,0);
+    }*/
